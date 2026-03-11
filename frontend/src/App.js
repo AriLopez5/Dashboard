b3098662745dbef50f0cd177e0c189638ce14a0d
AriLopez5 <ariadna.lopez5@educa.madrid.org>
2026-03-11 22:00:12 +0100

    Refactor cargarTodosDatos to remove useCallback

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -89,9 +89,9 @@ function AppContent() {
     }
   };
 
-  const cargarTodosDatos = useCallback(async (uid) => {
+  const cargarTodosDatos = async (uid) => {
     await Promise.all([cargarGastos(uid), cargarEntrenamientos(uid)]);
-  }, []);
+  };
   
   // Cargar datos cuando tengamos el email
   useEffect(() => {
