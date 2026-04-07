6c1c9a80886489091c1eaaac7effea61111e7ced
AriLopez5 <ariadna.lopez5@educa.madrid.org>
2026-04-07 10:17:29 +0200

    Corrección de Register
    Corrección del import de RegisterPage

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { CognitoUserPool, CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
+import { CognitoUserPool, CognitoUser } from 'amazon-cognito-identity-js';
 import cognitoConfig from '../auth/cognitoConfig';
 
 const userPool = new CognitoUserPool({
